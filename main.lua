bf27dc4be08d5c84dce8a93c2750b8e1919ae90c
pandaauon <pandaauonauon@gmail.com>
2026-01-02 05:55:55 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,11 +3,11 @@ if pickup == nil then
     pickup = false
 end
 print(pickup)
-
-repeat task.wait() until game:IsLoaded()
+---
+repeat wait() until game:IsLoaded()
 
 if game.PlaceId == 7922773201 then
-    task.wait(1)
+    wait(1)
     local args = {
         [1] = "SlotClick";
         [2] = game.Players.LocalPlayer:WaitForChild("PlayerGui", 9e9)
@@ -16,206 +16,254 @@ if game.PlaceId == 7922773201 then
             :WaitForChild("ScrollingFrame", 9e9)
             :WaitForChild("slot1", 9e9);
     }
+
     game:GetService("ReplicatedStorage"):WaitForChild("InputMainMenu", 9e9):FireServer(unpack(args))
-    game:GetService("ReplicatedStorage"):WaitForChild("InputMainMenu", 9e9):FireServer("QuickPlay")
-    return
-end
 
-local Webhook = "https://discord.com/api/webhooks/1456209792732496072/wbKOswpDJt5CEjqNRKVTpFIT0_o2b6EANsNjm4DliAoau8xBonaeRUWlKc2qrNymEWzQ"
+    local args2 = {
+        [1] = "QuickPlay";
+    }
 
-local Players = game:GetService("Players")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local HttpService = game:GetService("HttpService")
-local RunService = game:GetService("RunService")
+    game:GetService("ReplicatedStorage"):WaitForChild("InputMainMenu", 9e9):FireServer(unpack(args2))
+else
+	local function makepart(pos)
+		local part = Instance.new("Part")
+		part.Size = Vector3.new(10, 1, 10)
+		part.Anchored = true
+		part.CanCollide = true
+		part.Name = "StandPart"
+		part.CFrame = CFrame.new(pos.X, pos.Y - 3, pos.Z)
+		part.Parent = workspace
+	end
 
-local LocalPlayer = Players.LocalPlayer
-local PlayerGUI = LocalPlayer.PlayerGui
-local ServerTP = ReplicatedStorage:WaitForChild("ServerTP")
-local ServersFolder = ReplicatedStorage:WaitForChild("Servers")
+    local Webhook = 'https://discord.com/api/webhooks/1456209792732496072/wbKOswpDJt5CEjqNRKVTpFIT0_o2b6EANsNjm4DliAoau8xBonaeRUWlKc2qrNymEWzQ'
+    local Requests = game.ReplicatedStorage:WaitForChild("Requests")
+    local InteractRemote = Requests:WaitForChild("Interact")
 
-local Requests = ReplicatedStorage:WaitForChild("Requests")
-local InteractRemote = Requests:WaitForChild("Interact")
+    local ReplicatedStorage = game:GetService('ReplicatedStorage')	
+    local Players = game:GetService('Players')
+    local HttpService = game:GetService('HttpService')
 
-repeat task.wait() until LocalPlayer.Character
-task.wait(0.5)
+    local ServerTP = ReplicatedStorage:WaitForChild('ServerTP')
+    local LocalPlayer = Players.LocalPlayer
+    local PlayerGUI = LocalPlayer.PlayerGui
+    local HUD = PlayerGUI:WaitForChild('HUD')
+    repeat task.wait() until LocalPlayer.Character
+    task.wait(0.5)
+    local Input = LocalPlayer:WaitForChild('Backpack'):WaitForChild('Input')
 
-local Input = LocalPlayer.Backpack:WaitForChild("Input")
 
-Input:FireServer("usemove",{
-    ["HotbarGui"] = PlayerGUI.HUD.Hotbar.Bar["1"],
-    ["mousehit"] = CFrame.new(1,1,1)
-})
 
-local function NotifyWebhook(msg)
-    request({
-        Url = Webhook,
-        Method = "POST",
-        Headers = {["Content-Type"] = "application/json"},
-        Body = HttpService:JSONEncode({
-            content = tostring(msg),
-            embeds = {{
-                description =
-                    "Job ID = "..game.JobId..
-                    " username = "..LocalPlayer.Name..
-                    " profile = https://www.roblox.com/users/"..LocalPlayer.UserId.."/profile "..
-                    " servername = "..tostring(workspace:GetAttribute("ServerName")),
-                type = "rich",
-                color = tonumber(0xFF0000),
-                footer = { text = "panda" }
-            }}
-        })
-    })
-end
 
-local function pick(pos)
-    local closestModel
-    local closestDist = math.huge
-    for _, obj in ipairs(workspace:GetChildren()) do
-        if obj:IsA("Model") and obj.PrimaryPart then
-            local dist = (obj.PrimaryPart.Position - pos).Magnitude
-            if dist <= 5 and dist < closestDist then
-                closestDist = dist
-                closestModel = obj
-            end
-        end
-    end
-    if closestModel then
-        InteractRemote:FireServer(closestModel, nil)
-    end
-end
+	local function pick(pos)
+		local radius = 5
+		local closestModel
+		local closestDist = math.huge
 
-local noclipa
-local function noclip()
-    if noclipa then return end
-    noclipa = RunService.Stepped:Connect(function()
-        for _,v in ipairs(LocalPlayer.Character:GetDescendants()) do
-            if v:IsA("BasePart") then
-                v.CanCollide = false
-            end
-        end
-    end)
-end
+		for _, obj in ipairs(workspace:GetChildren()) do
+			if obj:IsA("Model") and obj.PrimaryPart then
+				local dist = (obj.PrimaryPart.Position - pos).Magnitude
+				if dist < closestDist and dist <= radius then
+					closestDist = dist
+					closestModel = obj
+				end
+			end
+		end
 
-local FOLDER = "raikoufinalstandshit"
-local FILE = FOLDER.."/Settings.json"
-if not isfolder(FOLDER) then makefolder(FOLDER) end
-if not isfile(FILE) then writefile(FILE, "{}") end
+		if closestModel then
+			InteractRemote:FireServer(closestModel, nil)
+		end
+	end
 
-local function LoadVisited()
-    return HttpService:JSONDecode(readfile(FILE))
-end
+	local TweenService = game:GetService("TweenService")
 
-local function SaveVisited(t)
-    writefile(FILE, HttpService:JSONEncode(t))
-end
+    local RunService = game:GetService("RunService")
+    local noclipa
 
-local function GetActiveJobIds()
-    local result = {}
-    local cursor
-    repeat
-        local url = "https://games.roblox.com/v1/games/"..game.PlaceId.."/servers/Public?limit=100"
-        if cursor then url = url.."&cursor="..cursor end
-        local res = request({Url = url, Method = "GET"})
-        if not res or res.StatusCode ~= 200 then break end
-        local data = HttpService:JSONDecode(res.Body)
-        for _,s in ipairs(data.data) do
-            result[s.id] = true
-        end
-        cursor = data.nextPageCursor
-        task.wait(0.4)
-    until not cursor
-    return result
-end
-
-function Teleport()
-    local active = GetActiveJobIds()
-    local visited = LoadVisited()
-
-    for id in pairs(visited) do
-        if not active[id] then
-            visited[id] = nil
-        end
-    end
-
-    for id in pairs(active) do
-        if visited[id] == nil then
-            visited[id] = false
-        end
-    end
-
-    local chosen
-    for id, used in pairs(visited) do
-        if used == false then
-            chosen = id
-            break
-        end
-    end
-
-    if not chosen then
-        for id in pairs(visited) do
-            visited[id] = false
-        end
-        SaveVisited(visited)
-        return Teleport()
-    end
-
-    local inst = ServersFolder:FindFirstChild(chosen)
-    if not inst then
-        visited[chosen] = true
-        SaveVisited(visited)
-        return Teleport()
-    end
-
-    visited[chosen] = true
-    SaveVisited(visited)
-
-    for i = 1, 5 do
-        ServerTP:FireServer(inst)
-        task.wait(0.25)
-    end
-    task.wait(2)
-    Teleport()
-end
-
-local function makepart(pos)
-    local part = Instance.new("Part")
-    part.Size = Vector3.new(10, 1, 10)
-    part.Anchored = true
-    part.CanCollide = true
-    part.Name = "StandPart"
-    part.CFrame = CFrame.new(pos.X, pos.Y - 3, pos.Z)
-    part.Parent = workspace
-end
-
-task.spawn(function()
-    local Found = false
-    local RecieveRemote = PlayerGUI:WaitForChild("RadarGui"):WaitForChild("Recieve")
-
-    RecieveRemote.OnClientEvent:Connect(function(Arg1, Arg2)
-        local Table1 = Arg1 or {}
-        local Table2 = Arg2 or {}
-        for _,tbl in ipairs({Table1, Table2}) do
-            for _,pos in ipairs(tbl) do
-                Found = true
-                NotifyWebhook("dragon radar found ball trying to teleport")
-                makepart(pos)
-                noclip()
-                LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(pos)
-                if pickup == true then
-                    task.wait(2)
-                    pick(pos)
-                    task.wait(0.2)
-                    pick(pos)
-                    LocalPlayer.Character:BreakJoints()
-                    LocalPlayer.CharacterAdded:Wait():WaitForChild("HumanoidRootPart")
-                    Teleport()
+    local function noclip()
+        if noclipa then return end
+        noclipa = RunService.Stepped:Connect(function()
+            for _,v in ipairs(LocalPlayer.Character:GetDescendants()) do
+                if v:IsA("BasePart") then
+                    v.CanCollide = false
                 end
             end
-        end
-    end)
-
-    task.wait(2)
-    if not Found then
-        Teleport()
+        end)
     end
-end)
+
+
+
+
+
+	Input:FireServer('usemove',{
+		["HotbarGui"] = PlayerGUI.HUD.Hotbar.Bar["1"],
+		["mousehit"] = CFrame.new(1,1,1)
+	})
+
+	local NotifyWebhook = function(msg)
+		local Message = {
+			["content"] = tostring(msg),
+			["embeds"] = {{
+				["description"] = 'Job ID = '..game.JobId..' username = '..LocalPlayer.Name..' profile = https://www.roblox.com/users/'..LocalPlayer.UserId..'/profile '.. 'servername = '..workspace:GetAttribute('ServerName'),
+				["type"] = "rich",
+				["color"] = tonumber(0xFF0000),
+				["footer"] = {
+					["text"] = 'panda'
+				}
+
+			}}
+		}
+		request({
+			Url = Webhook,
+			Method = 'POST',
+			Headers = {
+				['Content-Type'] = 'application/json'
+			},
+			Body = game:GetService('HttpService'):JSONEncode(Message)
+		});
+	end
+
+	local Trisu_Folder = isfolder("raikoufinalstandshit")
+	local Trisu_Settings = isfile("raikoufinalstandshit/Settings.json")
+
+	if Trisu_Folder then
+		if not Trisu_Settings then
+			local Table = {}
+			for _,ServerID in pairs(ReplicatedStorage.Servers:GetChildren()) do
+				if ServerID.Name == game.JobId then
+					Table[ServerID.Name] = true
+				else
+					Table[ServerID.Name] = false
+				end
+			end
+			writefile("raikoufinalstandshit/Settings.json", HttpService:JSONEncode(Table))
+		end
+	else
+		local Table = {}
+		for _,ServerID in pairs(ReplicatedStorage.Servers:GetChildren()) do
+			if ServerID.Name == game.JobId then
+				Table[ServerID.Name] = true
+			else
+				Table[ServerID.Name] = false
+			end
+		end
+		makefolder("raikoufinalstandshit")
+		writefile("raikoufinalstandshit/Settings.json",HttpService:JSONEncode(Table))
+	end
+	local function SyncServers()
+		if not isfile("raikoufinalstandshit/Settings.json") then return end
+
+		local Settings = HttpService:JSONDecode(readfile("raikoufinalstandshit/Settings.json"))
+		local CurrentServers = {}
+
+		-- Build current server list
+		for _, server in ipairs(ReplicatedStorage.Servers:GetChildren()) do
+			CurrentServers[server.Name] = true
+		end
+
+		-- 1. Remove old servers (no longer exist)
+		for serverId in pairs(Settings) do
+			if not CurrentServers[serverId] then
+				Settings[serverId] = nil
+			end
+		end
+
+		-- 2. Add new servers
+		for serverId in pairs(CurrentServers) do
+			if Settings[serverId] == nil then
+				Settings[serverId] = false
+			end
+		end
+
+		writefile(
+			"raikoufinalstandshit/Settings.json",
+			HttpService:JSONEncode(Settings)
+		)
+	end
+
+	local function Teleport()
+		SyncServers()
+		if not isfile("raikoufinalstandshit/Settings.json") then return end
+		
+		local Settings = HttpService:JSONDecode(readfile("raikoufinalstandshit/Settings.json"))
+
+		local ChosenID
+		for ServerID, Visited in pairs(Settings) do
+			if not Visited then
+				ChosenID = ServerID
+				Settings[ServerID] = true
+				break
+			end
+		end
+
+		if not ChosenID then
+			warn("resetting list")
+
+			for ServerID in pairs(Settings) do
+				Settings[ServerID] = false
+			end
+
+			writefile("raikoufinalstandshit/Settings.json", HttpService:JSONEncode(Settings))
+			return Teleport()
+		end
+
+		writefile("raikoufinalstandshit/Settings.json", HttpService:JSONEncode(Settings))
+
+		local FoundServer = ReplicatedStorage.Servers:FindFirstChild(ChosenID)
+		if not FoundServer then
+			warn("Server ID not found:", ChosenID)
+			return Teleport()
+		end
+
+		for i = 1, 5 do
+			ServerTP:FireServer(FoundServer)
+			task.wait(0.25)
+		end
+
+		print("Teleport request sent:", ChosenID)
+		task.wait(2)
+		Teleport()
+	end
+
+
+	task.spawn(function()
+		local Found = false
+		local RecieveRemote = PlayerGUI:WaitForChild('RadarGui'):WaitForChild('Recieve')
+
+		RecieveRemote.OnClientEvent:Connect(function(Arg1,Arg2)
+			local Table1,Table2 
+
+			Table1 = Arg1 or {}
+			Table2 = Arg2 or {}
+
+			for _, v26 in ipairs({Table1, Table2}) do
+				for _, v27 in v26 do
+					Found = true
+					NotifyWebhook('dragon radar found ball trying to teleport')
+					print(v27)
+					noclip()
+					game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(v27)
+                    if pickup == true then
+						makepart(v27)
+						local bodyVelocity = Instance.new("BodyVelocity")
+						bodyVelocity.Name = "WRDBodyVelocity"
+						bodyVelocity.Parent = player.Character.HumanoidRootPart
+                    	repeat task.wait() until game:IsLoaded()
+                        task.wait(2)
+                        pick(v27)
+                        task.wait(0.2)
+                        pick(v27)
+                        game.Players.LocalPlayer.Character:BreakJoints()
+                        game.Players.LocalPlayer.CharacterAdded:Wait():WaitForChild("HumanoidRootPart")
+                        Teleport()
+                    end
+				end
+			end
+		end)
+
+		task.wait(2)
+
+		if not Found then
+			Teleport()
+		end
+	end)
+end
